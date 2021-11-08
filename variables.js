cf5111d3adc6854b0c2f95c37a71b931bccfc3cd
Ronisha30 <ronisha.isham@yahoo.com>
2021-11-08 16:12:24 -0600

    prcticed combining strings

--- variables.js
+++ variables.js
@@ -2,7 +2,7 @@ let make = 'Nissan';
 console.log ( 'make', make);
 
 let model = 'Sentra'; 
-console.log ( 'nmodel', model);
+console.log ( 'model', model);
 
 let year = '2015'; 
 console.log ( 'year', year);
@@ -17,4 +17,8 @@ let engine = 'Gas';
 console.log ('engine', engine);
 
 let cylinders = 'inline 4'
-console.log ( cylinders, cylinders);
+console.log ( "cylinders", cylinders);
+
+// Two different ways to combine strings.
+console.log (`My car is a ${year} ${make} ${model} ${color} `) // This is a template literal.
+console.log("My car is a"+" " + year+" " + make+ " " + model+ " " + color) // This is combining variables with strings.
